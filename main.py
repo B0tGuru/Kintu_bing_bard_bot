4c2d7ca62205b72e1e90c69ffdfa92e1db25fa6b
aivan2798 <cot2we@gmail.com>
2023-11-13 00:43:07 +0300

    async still failed

--- main.py
+++ main.py
@@ -80,8 +80,8 @@ async def telegram_webhook(request: Request):
     #aibot.logs.append(":update_ends_here:")
     #aibot.logs.append(f"updates size: {updates_len}")
     #await 
-    #await aibot.tapp.process_update(update_in)
-    asyncio.create_task((aibot.tapp.process_update(update_in)))
+    await aibot.tapp.process_update(update_in)
+    #asyncio.create_task((aibot.tapp.process_update(update_in)))
     #handle_update(update)
     return {"status": "ok"}
 
